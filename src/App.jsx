5c302ee8595910456de01581250d3738e5ed35a9
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-12-03 20:15:30 +0530

    Enhance routing in App component with comments and 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import TaskDashboard from "./components/TaskDashboard";
-
-import TaskDetails from "./components/TaskDetails"; // Import the TaskDetails component
+import TaskDetails from "./components/TaskDetails";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/tasks"  />} />
+        {/* Redirect root path to /tasks */}
+        <Route path="/" element={<Navigate to="/tasks" />} />
+
+        {/* Task Dashboard */}
         <Route path="/tasks" element={<TaskDashboard />} />
-        <Route path="/tasks/:id" element={<TaskDetails />} /> {/* Task Details Page */}
+
+        {/* Task Details */}
+        <Route path="/tasks/:id" element={<TaskDetails />} />
+
+        {/* 404 Not Found */}
+        <Route path="*" element={<div className="text-center text-white p-6">404: Page Not Found</div>} />
       </Routes>
     </Router>
   );
